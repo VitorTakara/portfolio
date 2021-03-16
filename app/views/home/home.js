eb2fb9980241a7acea4f792c5401ac341bc3d0d9
Takara <vtakaraa@gmail.com>
2021-03-15 22:09:44 -0300

    Master Page + Menu modifier criados!

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,5 +1,29 @@
 function initHome(){
-    console.log('DEPENDENCIES HERE!!')
+    var options = {
+        strings: [
+            'Bem vindo_',
+            'Bienvenue_',
+            'Welcome_',
+            'ようこそ_',
+            'Benvenuto_',
+            'Willkommen_',
+            'Добро пожаловать_',
+            'Dobrodošli_',
+            '환영합니다_',
+            'Välkommen_',
+            'Welkom_',
+            '欢迎_'
+
+
+          ],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000,
+        loop: true,
+        loopCount: Infinity,
+      };
+      
+      var typed = new Typed('.welcome', options);
 }
 
 function setView(id){
--- app/views/work/work.js
+++ app/views/work/work.js
@@ -1,3 +1,3 @@
 function initWork(){
-    console.log('DEPENDENCIES HERE!!')
+    
 }
